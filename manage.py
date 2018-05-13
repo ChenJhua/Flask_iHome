84eec08b60ca5d01989cfd81390348988c1bb1f9
CJH <370619197@qq.com>
2018-05-13 10:16:55 +0800

    开启csrf保护

--- manage.py
+++ manage.py
@@ -2,6 +2,7 @@
 import redis
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
 
 
 class Config(object):
@@ -18,7 +19,6 @@ class Config(object):
     REDIS_PORT = 6379
 
 
-
 app = Flask(__name__)
 app.config.from_object(Config)
 
@@ -27,9 +27,12 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 # 创建redis连接对象
 redis_store = redis.StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
+# 开启CSRF保护
+# 只做保护校验：至于生成csrf_token cookie还有请求时携带csrf_token需要自己来完成
+CSRFProtect(app)
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
     # 测试redis
     redis_store.set("name", "itcast")
