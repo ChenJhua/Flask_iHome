d8fed87bb582865a14e7028e5cf60a948e744ed0
CJH <370619197@qq.com>
2018-05-13 11:48:59 +0800

    session信息的存储

--- manage.py
+++ manage.py
@@ -1,8 +1,9 @@
 # coding=utf-8
 import redis
-from flask import Flask
+from flask import Flask, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
+from flask_session import Session
 
 
 class Config(object):
@@ -17,6 +18,16 @@ class Config(object):
     # redis数据库配置
     REDIS_HOST = "192.168.158.136"
     REDIS_PORT = 6379
+    SECRET_KEY = "1231KJ!@#!@#!#!&$%!@^%#!@^$!@^$!(^$*(!@*(&#)!*&)&"
+
+    # session存储配置，设置session信息存储到redis数据库中
+    SESSION_TYPE = "redis"
+    # 设置session存储到那个redis数据库中
+    SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
+    # 开启session签名，加密
+    # SESSION_USE_SIGNER = True
+    # 设置session过期时间
+    PERMANENT_SESSION_LIFETIME = 86400 * 2
 
 
 app = Flask(__name__)
@@ -31,11 +42,17 @@ redis_store = redis.StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
 # 只做保护校验：至于生成csrf_token cookie还有请求时携带csrf_token需要自己来完成
 CSRFProtect(app)
 
+# session信息存储
+Session(app)
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     # 测试redis
-    redis_store.set("name", "itcast")
+    # redis_store.set("name", "itcast")
+
+    # 测试session存储
+    session["name"] = "itheima"
     return "index"
 
 
