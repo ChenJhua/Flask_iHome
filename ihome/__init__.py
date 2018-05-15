ed4d7a2c260c9d72dc64dddbdb5e905af4964d5e
CJH <370619197@qq.com>
2018-05-15 09:51:34 +0800

    解决循环导入，让一方延迟导入

--- ihome/__init__.py
+++ ihome/__init__.py
@@ -6,10 +6,12 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
 
 from config import config_dict
-from ihome.api_1_0.index import api
+
 
 # 创建SQLAlchemy对象
 db = SQLAlchemy()
+
+redis_store = None
 
 
 # 工厂方法
@@ -21,6 +23,7 @@ def create_app(config_name):
     # db对象进行app关联
     db.init_app(app)
     # 创建redis连接对象
+    global redis_store
     redis_store = redis.StrictRedis(host=config_cls.REDIS_HOST, port=config_cls.REDIS_PORT)
     # 开启CSRF保护
     # 只做保护校验：至于生成csrf_token cookie还有请求时携带csrf_token需要自己来完成
@@ -29,6 +32,7 @@ def create_app(config_name):
     # session信息存储
     Session(app)
 
+    from ihome.api_1_0 import api
     # 3.注册蓝图对象
     app.register_blueprint(api)
 
