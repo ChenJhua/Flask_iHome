6ec82699fd8a028ef46eb587a2ad9beddfe48ad5
CJH <370619197@qq.com>
2018-05-16 10:51:23 +0800

    发送短信验证码后端api接口代码实现

--- ihome/api_1_0/verify.py
+++ ihome/api_1_0/verify.py
@@ -1,5 +1,10 @@
 # coding=utf-8
 # 次文件中api用于提供图片验证码和短信验证码
+import json
+import re
+import random
+
+from ihome import constants
 from . import api
 from ihome import redis_store
 from ihome.utils.captcha import captcha
@@ -7,6 +12,62 @@ from ihome.constants import IMAGE_CODE_REDIS_EXPIRES
 from flask import make_response, request, jsonify
 from ihome.utils.response_code import RET
 from flask import current_app
+from ihome.utils.sms import CCP
+
+
+@api.route("/sms_code", method=["POST"])
+def send_sms_code():
+    """
+    发送短信验证码：
+    1.接收参数（手机号，图片验证码，图片验证码标始）并进行参数校验
+    2.从redis获取图片验证，如果取不到，说明图片验证码已经过期
+    3.对比图片验证码，如果一致
+    4.使用云通讯发送短信验证码
+    5.返回应答，发送短信成功
+    :return:
+    """
+    # 1.接收参数（手机号，图片验证码，图片验证码标始）并进行参数校验
+    # req_data = request.data
+    # req_dict = json.load(req_data)
+    # req_dict = request.get_json()
+    req_dict = request.json
+    mobile = req_dict.get("mobile")
+    image_code = req_dict.get("image_code")
+    image_code_id = req_dict.get("image_code_id")
+    if not all([mobile, image_code, image_code_id]):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数不完整")
+    if not re.match(r"^1[356789]\d{9}", mobile):
+        return jsonify(errno=RET.PARAMERR, errmsg="手机号格式不正确")
+    # 2.从redis获取图片验证，如果取不到，说明图片验证码已经过期
+    try:
+        real_image_code = redis_store.get("imagecode:%s" % image_code_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="获取图片验证失败")
+    if not real_image_code:
+        return jsonify(errno=RET.NODATA, errmsg="图片验证码已过期")
+    # 3.对比图片验证码，如果一致
+    if real_image_code != image_code:
+        return jsonify(errno=RET.DATAERR, errmsg="图片验证码错误")
+    # 4.使用云通讯发送短信验证码
+    # 4.1随机生成一个6位短信验证码
+    sms_code = "%06s" % random.randint(0, 999999)
+    try:
+        res = CCP().send_template_sms(mobile, [sms_code, constants.SMS_CODE_REDIS_EXPIRES/60], 1)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.THIRDERR, errmsg="发送短信失败")
+    if res != 1:
+        # 发送短信验证码失败
+        return jsonify(errno=RET.THIRDERR, errmsg="发送短信验证码失败")
+    # 4.3 在redis中保存短信验证码内容
+    try:
+        redis_store.set("smscode:%s" % mobile, sms_code, constants.SMS_CODE_REDIS_EXPIRES)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="保存短信验证码失败")
+    # 5.返回应答，发送短信成功
+    return jsonify(errno=RET.OK, errmsg="发送短信验证码成功")
 
 
 @api.route("/image_code")
---
+++ ihome/utils/sms.py
@@ -0,0 +1,43 @@
+# coding = utf-8
+from ihome.libs.yuntongxun.CCPRestSDK import REST
+import ConfigParser
+
+accountSid = '8aaf070863399ed9016359ba4fed1500'
+# 说明：主账号，登陆云通讯网站后，可在控制台首页中看到开发者主账号ACCOUNT SID。
+
+accountToken = '7fb853c525834e67bc8337d7299673ed'
+# 说明：主账号Token，登陆云通讯网站后，可在控制台首页中看到开发者主账号AUTH TOKEN。
+
+appId = '8aaf070863399ed9016359ba50471507'
+# 请使用管理控制台中已创建应用的APPID。
+
+serverIP = 'app.cloopen.com'
+# 说明：请求地址，生产环境配置成app.cloopen.com。
+
+serverPort = '8883'
+# 说明：请求端口 ，生产环境为8883.
+
+softVersion = '2013-12-26'  # 说明：REST API版本号保持不变。
+
+
+class CCP(object):
+    def __init__(self):
+        # 初始化REST SDK
+        self.rest = REST(serverIP, serverPort, softVersion)
+        self.rest.setAccount(accountSid, accountToken)
+        self.rest.setAppId(appId)
+
+    def send_template_sms(self, to, datas, tempId):
+
+        result = self.rest.sendTemplateSMS(to, datas, tempId)
+        if result.get("statusCode") == "000000":
+            return 1
+        else:
+            return 0
+        # print result
+        # for k, v in result.iteritems():
+        #     if k == 'templateSMS':
+        #         for k, s in v.iteritems():
+        #             print '%s:%s' % (k, s)
+        #     else:
+        #         print '%s:%s' % (k, v)
